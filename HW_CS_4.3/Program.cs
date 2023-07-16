0d337193222cfb36aeaa650c8b33f7eee0e14b7b
DmitryZinoviev <tg@dizi25>
2023-07-16 16:32:15 +0300

    Задание №4.3 выполнено

---
+++ HW_CS_4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 50); 
+}
+Console.Clear();
+Console.Write("Слйчайный массив из 8 чисел: ");
+int n = 8;
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
