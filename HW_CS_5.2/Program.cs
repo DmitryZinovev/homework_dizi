039e7cedf25a908d461584eb7ad8055bb750731e
DmitryZinoviev <tg@dizi25>
2023-07-16 17:03:56 +0300

    Задание №5.2 выполнено

---
+++ HW_CS_5.2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 10); 
+}
+
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+        sum = sum + array[i];
+        Console.Write($"{sum} ");
+
+}
+
+Console.Clear();
+//Console.Write("Введите кол-во элементов в массиве: ");
+int n = 4;
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
