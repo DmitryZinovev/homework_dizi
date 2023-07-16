723c56041e62be35f879bad5cfc5c876befffad2
DmitryZinoviev <tg@dizi25>
2023-07-16 15:42:51 +0300

    Задание 3.1 выполнено несколькими способами

--- HW_CS_3.1/Program.cs
+++ HW_CS_3.1/Program.cs
@@ -16,7 +16,7 @@
 //if (a*a == b || b*b == a)
 //    Console.WriteLine("y");
 //else Console.WriteLine("no");
-
+/*
 Console.Clear();
 int sum = 0;
 int t = 0;
@@ -37,3 +37,41 @@ Console.Write($"{t} не является палиндромом");
 }
 else
 Console.Write("Введите пятизначное число! ");
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10000; // 58369 / 10000 = 5
+int n2 = n / 1000 % 10; // 58369 / 1000 = 58 % 10 = 8
+int n3 = n / 100 % 10;
+int n4 = n / 10 % 10;
+int n5 = n % 10;
+if (n1 == n5 && n2 == n4)
+Console.Write($"{n} является палиндромом");
+else
+Console.Write($"{n} не является палиндромом");
+
+/*
+Console.Clear();
+string n = Console.ReadLine();
+if (n[0] == n[4]  && )
+
+
+Console.Clear();
+bool flag = true;
+while (flag == true)
+{
+    Console.Write("Введите число: ");
+    string n = Console.ReadLine();
+    if (n == "-")
+        flag = false;
+    else
+    {
+        if (n[0] == n[4] && n[1] == n[3])
+            Console.WriteLine("+");
+        else
+            Console.WriteLine("-");
+    }
+
+}*/
---
+++ HW_CS_4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+/*
+void f(int n, int s)
+{
+    int result = 1;
+    for (int i = 1; i <= s; i ++)
+        result *= n; // result = result * i
+    Console.WriteLine(result);
+    
+}
+*/
+Console.Clear();
+int i = 0;
+//int sum = 0;
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int s = Convert.ToInt32(Console.ReadLine());
+while (i < s)
+{
+   n = n * n;
+   i++;
+}
+Console.WriteLine(n);
