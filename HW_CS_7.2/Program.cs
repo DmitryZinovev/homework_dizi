87b50c2902a06f6b1b03cb3068145d86ab60297a
DmitryZinoviev <tg@dizi25>
2023-07-26 22:27:34 +0300

    Задание №7.2 выполнено

---
+++ HW_CS_7.2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+
+void InputArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите позицию элемента: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[4, 4];
+InputArray(matrix);
+Console.Write("\n");
+if ( matrix.GetLength(0) < size[0] ||  matrix.GetLength(1) < size[1])
+    Console.Write("такого числа в массиве нет");
+    else Console.Write(matrix[size[0], size[1]]);
+//SumPozichon(matrix);
