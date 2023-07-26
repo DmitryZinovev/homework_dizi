867de7d181cd3e5acb8e7974e168d8de74d49461
DmitryZinoviev <tg@dizi25>
2023-07-26 19:56:45 +0300

    Добавил решение преподавателя

--- HW_CS_5.3/Program.cs
+++ HW_CS_5.3/Program.cs
@@ -24,8 +24,25 @@ void ReleaseArray(int[] array)
 }
 
 Console.Clear();
-int n = 8;
-int[] array = new int[n];
+int[] array = new int[8];
 InputArray(array);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 ReleaseArray(array);
+
+/* пример преподавателя // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void InputArray(double[] array)
+{
+for (int i = 0; i < array.Length; i++)
+// array[i] = Math.Round(new Random().NextDouble() * 100, 2);
+array[i] = Math.Round(new Random().NextDouble(), 2) + new Random().Next(0, 1000);
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элемнетов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+*/
---
+++ HW_CS_6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+Console.Write("введите координаты прямых - ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+for (double i = 0; i < 10; i++ )
+{
+    if (array[i] > 0) count++;
+}
+Console.WriteLine(count);
